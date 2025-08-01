d4b427806d84e9daad5d8ba9f1ebe6570957a658
BunmiMary <bunmisadiku365@gmail.com>
2025-08-01 16:48:41 +0100

    Calculator

---
+++ index.js
@@ -0,0 +1,27 @@
+const display =  document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+} 
+
+function cleardisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+    display.value = eval(display.value)
+    }
+    catch(error){
+        display.value = "Error"
+    } 
+}
+
+
+
+
+var = keyItem {
+    "we": "index",
+}
+var ddd = keyItem["we"];
+
